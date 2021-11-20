60c6ab7623fb6f672aebc6afa9e4959690b9d99c
Vishal Prakash <v.r.prakash73@gmail.com>
2021-11-21 10:22:16 +1300

    added versioning to check image deployment version and set wait for reliability to true

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Body from './components/structure/body/body';
 function App() {
+
+  console.log('testing version of ecs deployment - this is version 1');
+
+
   return (
     <div className="App">
       <Body />
